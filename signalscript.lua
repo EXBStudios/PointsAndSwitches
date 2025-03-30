52b2a714cc2e9cdcba31c9b260c116e020bf545c
EXBStudios <103748542+EXBStudios@users.noreply.github.com>
2025-03-30 14:03:44 +0100

    Added already created SignalScript to GH for use
    Codespace for mac doesnt work for anything that isnt osx LMAO

---
+++ signalscript.lua
@@ -0,0 +1,17 @@
+local signal = -- Enter signal location
+local part = -- Enter DetectorPart location
+local parent = script.parent
+local signal1 = signal.Red1
+local signal2 = signal.Red2
+local signal3 = signal.Red3
+local signal4 = signal.Red4
+local SST = signal.SST.Value
+local NextSignal = -- Enter next signal location
+if NextSignal.SST.Value = red then
+    signal3.colour = 250, 167, 0
+    signal3.SurfaceLight.Enabled = true
+end
+part.function(hit):Connect(
+    signal4.colour = 187, 67, 60
+    signal4.SurfaceLight.Enabled = true
+end)
